fb6962f09d60be46714be568dfd9cd8ae4949382
nitta02 <nittaranjansarkertanmoy2022@gmail.com>
2024-10-14 21:45:27 +0600

    non commit

--- lib/presentation/pages/home/home.dart
+++ lib/presentation/pages/home/home.dart
@@ -1,19 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_shop_app/presentation/pages/home/bodySection/bodySection.dart';
-import 'package:grocery_shop_app/presentation/pages/home/upperSection/upperSection.dart';
+import 'package:grocery_shop_app/presentation/widgets/customTextFormField.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            UpperSection(),
-            BodySection(),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 12,
+            horizontal: 12,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Welcome',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                            Text('Tanmoy Sarker',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ],
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.amber,
+                          backgroundImage: NetworkImage(
+                              'https://img.freepik.com/premium-vector/young-man-avatar-character-vector-illustration-design_24877-18514.jpg'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const CustomTextFormFeild(
+                        hintText: 'Search for products',
+                        hintStyle: TextStyle(
+                          fontWeight: FontWeight.w200,
+                          fontSize: 14,
+                          letterSpacing: 1.8,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const BodySection(),
+              ],
+            ),
+          ),
         ),
       ),
     );
